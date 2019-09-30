99f842ec1406a6bde6414e4534cc6d9fb02a55aa
Hoang <cct.cry@gmai.com>
2019-09-30 14:29:17 +0700

    up file

---
+++ bai1.js
@@ -0,0 +1,5 @@
+let a =parseFloat(prompt("a: "));
+let b =parseFloat(prompt("b: "));
+result= ((a+b)<4) ?'below' : 'over';
+alert(result);
+
---
+++ bai2.js
@@ -0,0 +1,16 @@
+let login = prompt('nhap');
+switch (login)
+{ case 'Employee':
+    alert('Hello');
+    break;
+    case 'Driector':
+        alert('Greettings');
+        break;
+    case '':
+        alert('No  Login');
+        break;
+
+
+
+}
+
